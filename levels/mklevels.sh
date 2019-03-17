7e601c1643701d78a03c8efb801d9cd3b1445b1f
Dirk Reese <dirk@boa.local>
2019-03-17 08:54:22 +0100

    shell to create game levels

---
+++ levels/mklevels.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ln -s spheriod.txt level1.txt
+ln -s skycross.txt level2.txt
+ln -s skyisland.txt level3.txt
+ln -s twogwells.txt level4.txt
+ln -s twospheresAndChambers.txt level5.txt
